4ce3669ed1b3aad80c37f862258c3d9b4698408a
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2023-12-29 10:20:28 +0700

    Keep .exe extension except rescript executable

--- overlay.nix
+++ overlay.nix
@@ -8,20 +8,21 @@ in {
       url = "https://registry.npmjs.org/rescript/-/rescript-${version}.tgz";
       hash = "sha256-HWP9H5YsLoIfuD9XdE9bkAjSrs+vNcPdSIG66/ShdN0=";
     };
-    installPhase = ''
-      for bin in rescript bsc bsb_helper
-      do install -D ${
-        let
-          maybeArm64 = lib.optionalString stdenv.isAarch64 "arm64";
-        in
-          if stdenv.isLinux
-          then "linux" + maybeArm64
-          else if stdenv.isDarwin
-          then "darwin" + maybeArm64
-          else if stdenv.isCygwin || stdenv.isWindows
-          then "win32"
-          else throw "architecture not supported"
-      }/$bin.exe $out/bin/$bin
+    installPhase = let
+      dist = let
+        maybeArm64 = lib.optionalString stdenv.isAarch64 "arm64";
+      in
+        if stdenv.isLinux
+        then "linux" + maybeArm64
+        else if stdenv.isDarwin
+        then "darwin" + maybeArm64
+        else if stdenv.isCygwin || stdenv.isWindows
+        then "win32"
+        else throw "architecture not supported";
+    in ''
+      install -D ${dist}/rescript.exe $out/bin/rescript
+      for bin in bsc bsb_helper
+      do cp ${dist}/$bin.exe $out/bin/
       done
     '';
   };
